0667f686c963d3cf6144cfe208c09e28c5a07f16
hosimiya7 <nerokarurin@gmail.com>
2021-10-25 19:44:34 +0900

    span追加

--- resources/js/components/GameFoodShop.vue
+++ resources/js/components/GameFoodShop.vue
@@ -1,18 +1,28 @@
 <template>
-    <div class="message-game">
+  <div class="message-game">
+    <h3>▼ えさ販売店 現在{{ this.$store.state.shopPoint }}ポイント</h3>
 
-        <h3>▼ えさ販売店 現在{{ this.$store.state.shopPoint }}ポイント</h3>
-
-        <div class="game-index">
-            <div class="game-index-first">
-                <span v-bind:class="{active: this.$store.state.selectedSubCursor === 0}">スロット</span>
-                <span v-bind:class="{active: this.$store.state.selectedSubCursor === 1}">ハイ&amp;ロー</span>
-            </div>
-            <div class="game-index-second">
-                <span v-bind:class="{active: this.$store.state.selectedSubCursor === 2}">おみくじ</span>
-                <span v-bind:class="{active: this.$store.state.selectedSubCursor === 3}">えさ販売店</span>
-            </div>
-        </div>
-
+    <div class="game-index">
+      <div class="game-index-first">
+        <span v-bind:class="{active: this.$store.state.selectedSubCursor === 0}">
+          いちご
+          <span class="margin-none" v-text="">0</span>個
+        </span>
+        <span v-bind:class="{active: this.$store.state.selectedSubCursor === 1}">
+          おもち
+          <span class="margin-none" v-text="">0</span>個
+        </span>
+      </div>
+      <div class="game-index-second">
+        <span v-bind:class="{active: this.$store.state.selectedSubCursor === 2}">
+          めろん
+          <span class="margin-none" v-text="">0</span>個
+        </span>
+        <span v-bind:class="{active: this.$store.state.selectedSubCursor === 3}">
+          はっぱ
+          <span class="margin-none" v-text="">0</span>個
+        </span>
+      </div>
     </div>
+  </div>
 </template>
