00f088960f700bd1acaec15ce7df47faef5919ef
hosimiya7 <nerokarurin@gmail.com>
2021-12-05 17:33:02 +0900

    新規登録の人の目標作成

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -3,35 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use SebastianBergmann\Environment\Console;
 
 class GoalController extends Controller
 {
 
-     public function create(Request $request)
+    public function create(Request $request)
     {
+        // user_idが一致してない
         /**
          * @var User $user
          */
         $user = Auth::user();
-        $user->goal->fill($request->all())->save();
+        if ($user->goal === null) {
+            \App\Models\Goal::insert(
+                [
+                    'user_id' => $user->id,
+                    'goal' => $request->goal,
+                    'number' => $request->number,
+                    'unit' => $request->unit
+                ]
+            );
+        } else {
+            $user->goal->fill($request->all())->save();
+        }
 
         return $user->goal;
-
     }
 
     public function show(Request $request)
     {
+
         /**
          * @var User $user
          */
         $user = Auth::user();
-        $goal = $user->goal->first();
+        $goal = $user->goal;
 
         return $goal;
-
     }
-
 }
